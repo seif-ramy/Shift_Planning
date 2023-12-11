9031927e42f16af02e50ee3dc709b0f8aa3bcd4d
seif-ramy <85869949+seif-ramy@users.noreply.github.com>
2023-12-11 12:23:04 +0200

    employees data commit

--- srv/ShiftPlanning-service.cds
+++ srv/ShiftPlanning-service.cds
@@ -1,4 +1,5 @@
 using ECEmploymentInformation as ECEI_API from '../srv/external/ECEmploymentInformation.csn';
+using PLTUserManagement as UM_API from '../srv/external/PLTUserManagement.csn';
 
 namespace shift.planning.service;
 
@@ -11,12 +12,18 @@ service ShifPlan @(path : '/shiftplanning') {
              userId
         };
 
-    // @readonly
-    // entity SFSF_Specific_Manager_Employees as
-    //     select from ECEI_API.EmpJob {
-    //          userId
-    //     };
+    @readonly
+    entity SFSF_User as
+        select from UM_API.User {
+            key userId,
+                username,
+                defaultFullName,
+                email,
+                division,
+                department,
+                title
+        };
 
     annotate SFSF_Manager with @(cds.odata.valuelist);
-    // annotate SFSF_Specific_Manager_Employees with @(cds.odata.valuelist);
+    annotate SFSF_User with @(cds.odata.valuelist);
 }
--- srv/lib/handlers.js
+++ srv/lib/handlers.js
@@ -1,13 +1,41 @@
 const cds = require('@sap/cds');
+const { response } = require('express');
 
 let managerService = null;
+let userService = null;
 
 let sf_managers_array = [];
+let userIDs=[];
 
 (async function () {
     // Connect to external SFSF OData services
     managerService = await cds.connect.to('ECEmploymentInformation');
+    userService = await cds.connect.to('PLTUserManagement');
 })();
+
+/*** HELPERS ***/
+
+// Remove the specified columns from the ORDER BY clause of a SELECT statement
+function removeColumnsFromOrderBy(query, columnNames) {
+    if (query.SELECT && query.SELECT.orderBy) {
+        columnNames.forEach(columnName => {
+            // Look for column in query and its respective index
+            const element = query.SELECT.orderBy.find(column => column.ref[0] === columnName);
+            const idx = query.SELECT.orderBy.indexOf(element);
+
+            if (idx > -1) {
+                // Remove column from oder by list
+                query.SELECT.orderBy.splice(idx, 1);
+                if (!query.SELECT.orderBy.length) {
+                    // If list ends up empty, remove it from query
+                    delete query.SELECT.orderBy;
+                }
+            }
+        });
+    }
+
+    return query;
+}
 
 
 /*** HANDLERS ***/
@@ -27,10 +55,28 @@ async function readSFSF_Manager(req) {
             sf_managers_array[0] = sf_managers
         }
         
-        const userIDs = sf_managers_array.map(user => user.userId);
+        userIDs = sf_managers_array.map(user => user.userId);
         console.log(userIDs);
         return sf_managers_array
        
+    } catch (err) {
+        req.error(err.code, err.message);
+    }
+}
+
+// Read SFSF users
+async function readSFSF_User(req) {
+    try {
+        // Columns that are not sortable must be removed from "order by"
+        req.query = removeColumnsFromOrderBy(req.query, ['defaultFullName']);
+
+        // Handover to the SF OData Service to fecth the requested data
+        const tx = userService.tx(req);
+        req.query.where({ userId: { in: userIDs}});
+        let array = await tx.run(req.query);
+        console.log(array);
+        return await tx.run(req.query);
+        
     } catch (err) {
         req.error(err.code, err.message);
     }
@@ -60,6 +106,7 @@ async function readSFSF_Manager(req) {
 // }
 
 module.exports = {
-    readSFSF_Manager
+    readSFSF_Manager,
+    readSFSF_User
     // read_Specific_Manager
 }
